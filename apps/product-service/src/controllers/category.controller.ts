95888cae007643a01a35995743e6f5b7c3c7ead1
cristian2213 <crislm1322@gmail.com>
2026-01-08 05:19:06 -0500

    CRUD functions for category controller made

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -8,6 +8,37 @@ export const createCategory = async (req: Request, res: Response) => {
   return res.status(201).json(category)
 }
 
-export const updateCategory = async (req: Request, res: Response) => {}
-export const deleteCategory = async (req: Request, res: Response) => {}
-export const getCategories = async (req: Request, res: Response) => {}
+export const updateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const data: Prisma.CategoryUpdateInput = req.body
+  const categoryExists = await prisma.category.findUnique({
+    where: { id: Number(id) },
+  })
+  if (!categoryExists) {
+    return res.status(404).json({ message: 'Category not found' })
+  }
+
+  const category = await prisma.category.update({
+    where: { id: Number(id) },
+    data,
+  })
+
+  return res.status(200).json(category)
+}
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const categoryExists = await prisma.category.findUnique({
+    where: { id: Number(id) },
+  })
+  if (!categoryExists) {
+    return res.status(404).json({ message: 'Category not found' })
+  }
+  const category = await prisma.category.delete({ where: { id: Number(id) } })
+  return res.status(200).json(category)
+}
+
+export const getCategories = async (req: Request, res: Response) => {
+  const categories = await prisma.category.findMany()
+  return res.status(200).json(categories)
+}
